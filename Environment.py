90df9d2f99ca3501e97371b24fdf9608295d0096
Rylinn <114656169+RylinnM@users.noreply.github.com>
2023-04-27 16:35:02 +0200

    Update Environment.py

--- Environment.py
+++ Environment.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Practical for course 'Reinforcement Learning',
-Leiden University, The Netherlands
-By Thomas Moerland
+Written and modified by R.Ma
 """
 import matplotlib
 matplotlib.use('Qt5Agg') # 'TkAgg'
